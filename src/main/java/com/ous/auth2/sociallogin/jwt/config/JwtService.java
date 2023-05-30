f75a334d819874985e9f9b13f7fe682bca24c896
ousmane <sangaryousmane@gmail.com>
2023-05-30 18:58:48 +0800

    Added claims and Keys to configuration

--- src/main/java/com/ous/auth2/sociallogin/jwt/config/JwtService.java
+++ src/main/java/com/ous/auth2/sociallogin/jwt/config/JwtService.java
@@ -1,14 +1,34 @@
 package com.ous.auth2.sociallogin.jwt.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    private static final String  SECRET_KEY = "8EB8F962875843D7762E14FCF9B19";
 
 
     public String extractUserEmail(String token){
 
         return null;
+    }
+
+    public Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
     }
 }
