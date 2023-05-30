e122f36b22f7872defc743a393cef771abffec6d
ousmane <sangaryousmane@gmail.com>
2023-05-30 18:43:07 +0800

    Add dependencies for jwt implementation

---
+++ src/main/java/com/ous/auth2/sociallogin/jwt/config/JwtService.java
@@ -0,0 +1,14 @@
+package com.ous.auth2.sociallogin.jwt.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+
+
+    public String extractUserEmail(String token){
+
+        return null;
+    }
+}
