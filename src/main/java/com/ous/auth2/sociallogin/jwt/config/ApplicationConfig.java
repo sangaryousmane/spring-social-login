39c955b4bbd064c3cee91ba93cc4e84559dbf4f5
ousmane <sangaryousmane@gmail.com>
2023-05-31 12:23:12 +0800

    Configure UserDetailsService

---
+++ src/main/java/com/ous/auth2/sociallogin/jwt/config/ApplicationConfig.java
@@ -0,0 +1,19 @@
+package com.ous.auth2.sociallogin.jwt.config;
+import com.ous.auth2.sociallogin.jwt.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Sorry, user not found"));
+    }
+}
--- src/main/java/com/ous/auth2/sociallogin/jwt/config/JwtAuthenticationFilter.java
+++ src/main/java/com/ous/auth2/sociallogin/jwt/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +20,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -34,5 +38,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUserEmail(jwt);
+
+        // TODO: check if the user isn't authenticated yet
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails=this.userDetailsService.loadUserByUsername(userEmail);
+
+        }
     }
 }
